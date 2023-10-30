883dc85306349fd35e57c4bf2724e4b2ba5c0ed3
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-10-30 09:42:32 +0900

    15장 예제 임포트 수정

--- src/main/java/chapter15/book/chap15/ExecutorServiceExample.java
+++ src/main/java/chapter15/book/chap15/ExecutorServiceExample.java
@@ -1,24 +1,24 @@
 package chapter15.book.chap15;
 
-import static modernjavainaction.chap15.Functions.fo;
-import static modernjavainaction.chap15.Functions.go;
-
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import static chapter15.book.chap15.Functions.fo;
+import static chapter15.book.chap15.Functions.go;
+
 public class ExecutorServiceExample {
 
-  public static void main(String[] args) throws ExecutionException, InterruptedException {
-    int x = 1337;
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int x = 1337;
 
-    ExecutorService executorService = Executors.newFixedThreadPool(2);
-    Future<Integer> y = executorService.submit(() -> fo(x));
-    Future<Integer> z = executorService.submit(() -> go(x));
-    System.out.println(y.get() + z.get());
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<Integer> y = executorService.submit(() -> fo(x));
+        Future<Integer> z = executorService.submit(() -> go(x));
+        System.out.println(y.get() + z.get());
 
-    executorService.shutdown();
-  }
+        executorService.shutdown();
+    }
 
 }
--- src/main/java/chapter15/book/chap15/ThreadExample.java
+++ src/main/java/chapter15/book/chap15/ThreadExample.java
@@ -1,30 +1,31 @@
 package chapter15.book.chap15;
 
-import static modernjavainaction.chap15.Functions.f;
-import static modernjavainaction.chap15.Functions.g;
+
+import static chapter15.book.chap15.Functions.f;
+import static chapter15.book.chap15.Functions.g;
 
 class ThreadExample {
 
-  public static void main(String[] args) throws InterruptedException {
-    int x = 1337;
-    Result result = new Result();
+    public static void main(String[] args) throws InterruptedException {
+        int x = 1337;
+        Result result = new Result();
 
-    Thread t1 = new Thread(() -> {
-      result.left = f(x);
-    });
-    Thread t2 = new Thread(() -> {
-      result.right = g(x);
-    });
-    t1.start();
-    t2.start();
-    t1.join();
-    t2.join();
-    System.out.println(result.left + result.right);
-  }
+        Thread t1 = new Thread(() -> {
+            result.left = f(x);
+        });
+        Thread t2 = new Thread(() -> {
+            result.right = g(x);
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(result.left + result.right);
+    }
 
-  private static class Result {
-    private int left;
-    private int right;
-  }
+    private static class Result {
+        private int left;
+        private int right;
+    }
 
 }
