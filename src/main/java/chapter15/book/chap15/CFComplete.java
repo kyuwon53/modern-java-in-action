4a5cd5d490e8a82c4ad96c8af72eed1364b03e96
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-10-30 09:41:39 +0900

    15장 예제 제공 소스

---
+++ src/main/java/chapter15/book/chap15/CFComplete.java
@@ -0,0 +1,26 @@
+package chapter15.book.chap15;
+
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static chapter15.book.chap15.Functions.f;
+import static chapter15.book.chap15.Functions.g;
+
+public class CFComplete {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        int x = 1337;
+
+        CompletableFuture<Integer> a = new CompletableFuture<>();
+        executorService.submit(() -> a.complete(f(x)));
+        int b = g(x);
+        System.out.println(a.get() + b);
+
+        executorService.shutdown();
+    }
+
+}
