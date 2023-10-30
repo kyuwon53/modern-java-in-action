62be52c0e519dd20eb19ae463bf34f9d4fa75d7f
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-10-30 11:08:22 +0900

    Processor 생성

--- src/main/java/chapter17/TempMain.java
+++ src/main/java/chapter17/TempMain.java
@@ -4,8 +4,9 @@ import java.util.concurrent.Flow.Publisher;
 
 public class TempMain {
     public static void main(String[] args) {
-        getTemperaturesBasic("Basic New York").subscribe(new TempSubscriber());
-        getTemperaturesExecutor("Executor New York").subscribe(new TempSubscriber());
+//        getTemperaturesBasic("Basic New York").subscribe(new TempSubscriber());
+//        getTemperaturesExecutor("Executor New York").subscribe(new TempSubscriber());
+        getCelsiusTemperatures("New York").subscribe(new TempSubscriber());
     }
 
     private static Publisher<TempInfo> getTemperaturesBasic(String town) {
@@ -18,5 +19,13 @@ public class TempMain {
         return subscriber -> subscriber.onSubscribe(
                 new TempExecutorSubscription(subscriber, town)
         );
+    }
+
+    public static Publisher<TempInfo> getCelsiusTemperatures(String town) {
+        return subscriber -> {
+            TempProcessor processor = new TempProcessor();
+            processor.subscribe(subscriber);
+            processor.onSubscribe(new TempExecutorSubscription(processor, town));
+        };
     }
 }
